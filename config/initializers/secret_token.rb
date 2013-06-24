7e35532042a14c922386b3b02d54f10943d7a2d3
Bindesh Vijayan <bindeshvijayan@gmail.com>
2013-06-24 12:00:47 +0530

    adding repo for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapsSample::Application.config.secret_token = '68ea04b6298d517ed51a4db19109202f1892f342a484c10ac74eda460179ec15aa9a9aebfa8f3dbd45dec5f7b47331518f751fcf6230f4a445a77697b5f6574a'
